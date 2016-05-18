809eb221a0295d09a1f3c4b372034b2fa475c97a
mmarkvoort-Cyanide <mmarkvoort@outlook.com>
2016-05-18 16:21:17 +0200

    Update and rename cm.mk to cyanide.mk

--- cyanide.mk
+++ cyanide.mk
@@ -2,20 +2,20 @@
 PRODUCT_RELEASE_NAME := a7alte
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyanide/config/common_full_phone.mk)
 
 # Inherit nfc enhanced configuration
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyanide/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a7alte/device_a7alte.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_a7alte
+PRODUCT_NAME := cyanide_a7alte
 PRODUCT_DEVICE := a7alte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=a7altexx \
 	PRODUCT_MODEL=SM-A700F \
 	PRODUCT_MANUFACTURER=samsung \
-	TARGET_DEVICE=a8elte \
+	TARGET_DEVICE=a7alte \
